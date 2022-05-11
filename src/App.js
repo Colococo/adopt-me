7135a6f24aab2d3e2218d7b9e669431ea2585555
Colo <jorge_mattii@hotmail.com>
2022-05-11 18:00:54 -0300

    jsx y eslint plugins y useState hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
-    React.createElement(Pet, {
-      name: "Oscar",
-      animal: "Perro",
-      breed: "Callejero",
-    }),
-  ]);
-};
+
+
+  return (
+    <div>
+      <h1>Adpot me!</h1>
+      {/*<Pet name="Luna" animal="dog" breed="Havanese"/>
+      < name="Pepper" animal="Bird" breed="Cockatiel"/>
+      <Pet name="Doink" animal="Cat" breed="Mix"/> */}
+       <SearchParams />
+    </div>
+  )
+}
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//     <  name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Doink",
+//       animal: "Cat",
+//       breed: "Mix",>
+//     }),
+//     React.createElement(Pet, {
+//       name: "Oscar",
+//       animal: "Perro",
+//       breed: "Callejero",
+//     })
+//   ]);
+// };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const SearchParams = () => {
+
+
+    /**
+     * useState = HOOK 
+     * useState retorna un arreglo con dos cosas, el valor actual del estado y una function para actualizar el estado.
+     * Estamos utilizando una caractersitica de JavaScript que se llama DESCTRUCTURING (EXTRACCION) para obtener estas dos cosas
+     * (el valor actual y la funcion) del arreglo.
+     * 
+     * usamos la funcion setLocation el atributo onChange del INPUT, cada vez que escribimos en el input
+     * va a llamar a la funcion setLocation con lo que estamos escribiendo. 
+     * Cuando la funcion setLocaion es llamada, REACT sabe que su esetado fue modificado y lanza otro ciclo de renderizacion.
+     * 
+     * 
+     */
+    const [location, setLocation] = useState('Vicente Lopez');
+
+    // const location = 'Vicente Lopez';
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input id="location" value={location} placeholder="Location" onChange={(e)=> setLocation(e.target.value)} />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchParams;
+
